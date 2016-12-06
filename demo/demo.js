6a5707e952b8f15bc119eca7ea786a5c95d9c21a
vanpipy <vanpipy@gmail.com>
2016-12-06 22:53:24 +0800

    Optimize test used to find the right way to design the correct function in scroll.

--- demo/demo.js
+++ demo/demo.js
@@ -1,8 +1,16 @@
 
 (function(global, Handler) {
     var elem = document.getElementById('1');
-    console.log(elem.addEventListener);
     var handler = new Handler(elem);
 
-
+    handler.start(function() {
+        console.log('Custom start function');
+    });
+    handler.processing(function() {
+        console.log('Custom processing function');
+    });
+    handler.end(function() {
+        console.log('Custom end function');
+    });
+    handler.init();
 })(window, window.iwantScroll);
--- src/index.js
+++ src/index.js
@@ -3,40 +3,61 @@
 (function(global, handler) {
     global.iwantScroll = handler();
 })(window, function () {
-    //start, processing, end.
-    var iwantScroll = function() {};
-    var events = ['mousedown', 'mousemove', 'mouseup'];
+    var events = {
+        mouse: ['mousedown', 'mousemove', 'mouseup'],
+    };
 
     function isFunction (x) {
         return Object.prototype.toString.call(x) === '[object Function]';
     }
 
     function _start (callback) {
-        if (isFunction(callback)) {
-            callback();
-        }
         console.log('start');
     }
 
     function _processing (callback) {
-        if (isFunction(callback)) {
-            callback();
-        }
         console.log('processing');
     }
 
     function _end (callback) {
-        if (isFunction(callback)) {
-            callback();
-        }
         console.log('end');
     }
 
-    function _detection (event) {
-        workflow[ events.indexOf(event.type) ]();
+    function _startHandler (Fn) {
+        return function (event) {
+            var _element = this;
+
+            if (!_element._hasMouseMove) {
+                _element._hasMouseMove = true;
+                addEventListener(_element, 'mousemove', _processingHandler(Fn));
+            }
+
+            if (!_element._hasMouseUp) {
+                _element._hasMouseUp = true;
+                addEventListener(_element, 'mouseup', _endHandler(Fn));
+            }
+
+            Fn.start();
+        }
     }
 
-    function addEventLister (element, eventName, fn) {
+    function _processingHandler (Fn) {
+        return function (event) {
+            Fn.processing();
+        }
+    }
+
+    function _endHandler (Fn) {
+        return function (event) {
+            Fn.end();
+        }
+    }
+
+    function _init () {
+        addEventListener(this._target, 'mousedown', _startHandler(this));
+    }
+
+    function addEventListener (element, eventName, fn) {
         if (element.addEventListener) {
             element.addEventListener(eventName, fn, false);
         } else if (element.attachEvent) {
@@ -46,7 +67,7 @@
         }
     }
 
-    function removeEventLister (element, eventName, fn) {
+    function removeEventListener (element, eventName, fn) {
         if (element.removeEventListener) {
             element.removeEventListener(eventName, fn, false);
         } else if (element.detachEvent) {
@@ -57,15 +78,12 @@
         }
     }
 
-    var workflow = [_start, _processing, _end];
-
-    iwantScroll = function (element, options) {
-        addEventLister(element, 'mousedown', _detection);
-        addEventLister(element, 'mousemove', _detection);
-        addEventLister(element, 'mouseup', _detection);
+    var iwantScroll = function (element, options) {
+        this._target = element;
     };
 
     iwantScroll.prototype = {
+        init: _init,
         start: _start,
         processing: _processing,
         end: _end
