b2c28ecb975287ab321995dc0b314aa671493e04
vanpipy <vanpipy@gmail.com>
2016-12-06 10:54:06 +0800

    Add demo to test.

---
+++ demo/demo.js
@@ -0,0 +1,8 @@
+
+(function(global, Handler) {
+    var elem = document.getElementById('1');
+    console.log(elem.addEventListener);
+    var handler = new Handler(elem);
+
+
+})(window, window.iwantScroll);
--- src/index.js
+++ src/index.js
@@ -1,31 +1,44 @@
 'use strict';
 
 (function(global, handler) {
-
+    global.iwantScroll = handler();
 })(window, function () {
     //start, processing, end.
     var iwantScroll = function() {};
-    var events ['mousedown', 'mousemove', 'mouseup'];
+    var events = ['mousedown', 'mousemove', 'mouseup'];
 
-    function _start () {
-        
+    function isFunction (x) {
+        return Object.prototype.toString.call(x) === '[object Function]';
     }
 
-    function _processing () {
-        
+    function _start (callback) {
+        if (isFunction(callback)) {
+            callback();
+        }
+        console.log('start');
     }
 
-    function _end () {
-        
+    function _processing (callback) {
+        if (isFunction(callback)) {
+            callback();
+        }
+        console.log('processing');
+    }
+
+    function _end (callback) {
+        if (isFunction(callback)) {
+            callback();
+        }
+        console.log('end');
     }
 
     function _detection (event) {
-
+        workflow[ events.indexOf(event.type) ]();
     }
 
     function addEventLister (element, eventName, fn) {
-        if (element.addEventLister) {
-            element.addEventLister(eventName, fn, false);
+        if (element.addEventListener) {
+            element.addEventListener(eventName, fn, false);
         } else if (element.attachEvent) {
             element.attachEvent('on' + eventName, fn);
         } else {
@@ -34,8 +47,8 @@
     }
 
     function removeEventLister (element, eventName, fn) {
-        if (element.removeEventLister) {
-            element.removeEventLister(eventName, fn, false);
+        if (element.removeEventListener) {
+            element.removeEventListener(eventName, fn, false);
         } else if (element.detachEvent) {
             element.detachEvent('on' + eventName, fn);
         } else {
@@ -49,14 +62,14 @@
     iwantScroll = function (element, options) {
         addEventLister(element, 'mousedown', _detection);
         addEventLister(element, 'mousemove', _detection);
-        addEventLister(element, 'mouseup', _detection):
-    }
+        addEventLister(element, 'mouseup', _detection);
+    };
 
     iwantScroll.prototype = {
         start: _start,
         processing: _processing,
         end: _end
-    }
+    };
 
     return iwantScroll;
 });
